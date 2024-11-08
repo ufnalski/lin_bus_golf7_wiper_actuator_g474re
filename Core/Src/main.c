148739b87929cd3463683090cba87e628e6a2bd5
ufnalski <148026024+ufnalski@users.noreply.github.com>
2024-11-08 11:32:46 +0100

    Initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,533 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "i2c.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ssd1306.h"
+
+// https://github.com/nayuki/QR-Code-generator
+// https://www.nayuki.io/page/qr-code-generator-library
+#include "qrcodegen.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define LIN_SEND_PERIOD 80
+#define TX_BUFFER_SIZE 11
+#define RX_BUFFER_SIZE 10
+
+#define INT_MODE_ON 1
+#define INT_MODE_OFF 0
+#define INT_SPD1 1
+#define INT_SPD2 5
+#define INT_SPD3 9
+#define INT_SPD4 13
+
+#define SPD1_MODE_OFF 0
+#define SPD1_MODE_ON 1
+#define SPD2_MODE_OFF 0
+#define SPD2_MODE_ON 1
+
+#define SST_MODE_OFF 0
+#define SST_MODE_ON 1
+#define SST_MANUAL_INTERVAL 8000
+
+#define NUM_OF_MODES 9
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+const char *wiper_mode_name[] =
+{ "INT1", "INT2", "INT3", "INT4", "SPD1", "SPD2", "SST", "STOP", "OFF" };
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+// https://controllerstech.com/stm32-uart-8-lin-protocol-part-1/
+// https://controllerstech.com/stm32-uart-9-lin-protocol-part-2/
+// https://controllerstech.com/stm32-uart-10-lin-protocol-part-3/
+uint8_t Pid_Calc(uint8_t ID);
+uint8_t Checksum_Calc(uint8_t PID, uint8_t *data, uint8_t size);
+
+static void showPartDatasheet(void);
+static void printQr(const uint8_t qrcode[]);
+static void displayQr(const uint8_t qrcode[]);
+
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+PUTCHAR_PROTOTYPE
+{
+	HAL_UART_Transmit(&hlpuart1, (uint8_t*) &ch, 1, HAL_MAX_DELAY);
+	return ch;
+}
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+uint8_t txData[TX_BUFFER_SIZE];
+uint8_t rxData[RX_BUFFER_SIZE];
+
+uint32_t LinSendSoftTimer;
+
+uint8_t golf7counter = 0;
+
+uint8_t wiper_mode = 0;
+
+uint32_t rng_number;
+
+volatile uint8_t lin_data_received_flag = 0;
+
+uint8_t single_stroke_flag = 0;
+uint32_t SingleStrokeSoftTimer;
+
+extern DMA_HandleTypeDef hdma_usart3_rx;
+
+uint8_t lcd_line[64];
+uint32_t uart_message_length;
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_LPUART1_UART_Init();
+  MX_USART3_UART_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+
+	ssd1306_Init();
+
+	showPartDatasheet();
+	HAL_Delay(3000);
+
+	ssd1306_Fill(Black);
+	ssd1306_SetCursor(20, 2);
+	ssd1306_WriteString("ufnalski.edu.pl", Font_6x8, White);
+	ssd1306_SetCursor(18, 12);
+	ssd1306_WriteString("LIN 2.0 bus demo", Font_6x8, White);
+	ssd1306_SetCursor(13, 22);
+	ssd1306_WriteString("VW Golf VII wiper", Font_6x8, White);
+	ssd1306_UpdateScreen();
+
+	LinSendSoftTimer = HAL_GetTick();
+	SingleStrokeSoftTimer = HAL_GetTick();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1)
+	{
+
+		// manual trigger simulation
+		if (HAL_GetTick() - SingleStrokeSoftTimer > SST_MANUAL_INTERVAL)
+		{
+			SingleStrokeSoftTimer = HAL_GetTick();
+			single_stroke_flag = 0;
+		}
+
+		if (lin_data_received_flag == 1)
+		{
+			lin_data_received_flag = 0;
+//			HAL_UART_Transmit(&hlpuart1, rxData, RX_BUFFER_SIZE, 10);
+//			HAL_UART_Transmit(&hlpuart1, (uint8_t *)"\r\n", sizeof("\r\n"), 10);
+			uart_message_length = sprintf((char*) lcd_line,
+					"Rx %02X %02X %02X %02X %02X %02X", rxData[3], rxData[4],
+					rxData[5], rxData[6], rxData[7], rxData[8]);
+			HAL_UART_Transmit(&hlpuart1, lcd_line, uart_message_length, 10);
+			HAL_UART_Transmit(&hlpuart1, (uint8_t*) "\r\n", sizeof("\r\n"), 10);
+			ssd1306_SetCursor(2, 55);
+			ssd1306_WriteString((char*) lcd_line, Font_6x8, White);
+			ssd1306_UpdateScreen();
+		}
+
+		if (HAL_GetTick() - LinSendSoftTimer > LIN_SEND_PERIOD)
+		{
+			LinSendSoftTimer = HAL_GetTick();
+
+			if (golf7counter == 16)
+			{
+				golf7counter = 0;
+			}
+			// send status
+			txData[0] = 0x55;  // sync field
+
+			txData[1] = Pid_Calc(0x31);
+
+			txData[2] = (0x30 | (golf7counter & 0x0F)); // byte 0
+
+			switch (wiper_mode)
+			{
+			case 0: // INT1
+				txData[3] =
+						((SST_MODE_OFF << 4) | (INT_MODE_ON << 5)
+								| (SPD1_MODE_OFF << 6) | (SPD2_MODE_OFF << 7)
+								| INT_SPD1);
+				break;
+			case 1: // INT2
+				txData[3] =
+						((SST_MODE_OFF << 4) | (INT_MODE_ON << 5)
+								| (SPD1_MODE_OFF << 6) | (SPD2_MODE_OFF << 7)
+								| INT_SPD2);
+				break;
+			case 2: // INT3
+				txData[3] =
+						((SST_MODE_OFF << 4) | (INT_MODE_ON << 5)
+								| (SPD1_MODE_OFF << 6) | (SPD2_MODE_OFF << 7)
+								| INT_SPD3);
+				break;
+			case 3: // INT4
+				txData[3] =
+						((SST_MODE_OFF << 4) | (INT_MODE_ON << 5)
+								| (SPD1_MODE_OFF << 6) | (SPD2_MODE_OFF << 7)
+								| INT_SPD4);
+				break;
+			case 4: // SPD1 (slower)
+				txData[3] = ((SST_MODE_OFF << 4) | (INT_MODE_OFF << 5)
+						| (SPD1_MODE_ON << 6) | (SPD2_MODE_OFF << 7) | 0);
+				break;
+			case 5: // SPD2 (faster)
+				txData[3] = ((SST_MODE_OFF << 4) | (INT_MODE_OFF << 5)
+						| (SPD1_MODE_OFF << 6) | (SPD2_MODE_ON << 7) | 0);
+				break;
+			case 6: // SST
+				if (single_stroke_flag == 0)
+				{
+					single_stroke_flag = 1;
+					txData[3] = ((SST_MODE_ON << 4) | (INT_MODE_OFF << 5)
+							| (SPD1_MODE_OFF << 6) | (SPD2_MODE_OFF << 7) | 0);
+				}
+				else
+				{
+					txData[3] = ((SST_MODE_OFF << 4) | (INT_MODE_OFF << 5)
+							| (SPD1_MODE_OFF << 6) | (SPD2_MODE_OFF << 7) | 0);
+				}
+				break;
+			case 7: // STOP
+				txData[3] = ((SST_MODE_OFF << 4) | (INT_MODE_OFF << 5)
+						| (SPD1_MODE_OFF << 6) | (SPD2_MODE_OFF << 7) | 0);
+				break;
+			case 8: // OFF
+				txData[2] = (0x00 | (golf7counter & 0x0F));
+				txData[3] = 0x00;
+				break;
+			default:
+				txData[3] = 0x00;  // byte 1
+			}
+
+			txData[4] = 0x00;
+			txData[5] = 0x00;
+			txData[6] = 0x00;
+			txData[7] = 0x00;
+			txData[8] = 0x00;
+			txData[9] = 0x00; // byte 7
+
+			txData[10] = Checksum_Calc(txData[1], txData + 2, 8); // lin 2.0/2.1 includes PID
+
+			HAL_LIN_SendBreak(&huart3);
+			HAL_UART_Transmit(&huart3, txData, 11, 100);
+
+			ssd1306_SetCursor(2, 32);
+			sprintf((char*) lcd_line, "actuator mode %s ",
+					wiper_mode_name[wiper_mode]);
+			ssd1306_WriteString((char*) lcd_line, Font_6x8, White);
+
+			ssd1306_SetCursor(2, 45);
+			sprintf((char*) lcd_line, "Tx %02X %02X %02X %02X %02X %02X",
+					txData[2], txData[3], txData[4], txData[5], txData[6],
+					txData[7]);
+			ssd1306_WriteString((char*) lcd_line, Font_6x8, White);
+
+			ssd1306_UpdateScreen();
+
+			golf7counter++;
+
+			HAL_Delay(5);
+			// ask for diagnostics
+			txData[0] = 0x55;  // sync field
+			txData[1] = Pid_Calc(0x32);
+
+			txData[2] = Checksum_Calc(txData[1], txData + 2, 0); //lin 2.0/2.1 includes PID
+
+			memset(rxData, 0x00, RX_BUFFER_SIZE);
+			HAL_UARTEx_ReceiveToIdle_DMA(&huart3, rxData, RX_BUFFER_SIZE);
+			__HAL_DMA_DISABLE_IT(&hdma_usart3_rx, DMA_IT_HT);
+
+			HAL_LIN_SendBreak(&huart3);
+			HAL_UART_Transmit_DMA(&huart3, txData, 2);
+		}
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+  RCC_OscInitStruct.PLL.PLLN = 12;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV4;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if (GPIO_Pin == B1_Pin)
+	{
+		wiper_mode++;
+		wiper_mode %= NUM_OF_MODES;
+//		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	}
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (huart->Instance == USART3)
+	{
+		lin_data_received_flag = 1;
+//		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	}
+}
+
+uint8_t Pid_Calc(uint8_t ID)
+{
+	if (ID > 0x3F)
+		Error_Handler();
+	uint8_t IDBuf[6];
+	for (int i = 0; i < 6; i++)
+	{
+		IDBuf[i] = (ID >> i) & 0x01;
+	}
+
+	uint8_t P0 = (IDBuf[0] ^ IDBuf[1] ^ IDBuf[2] ^ IDBuf[4]) & 0x01;
+	uint8_t P1 = ~((IDBuf[1] ^ IDBuf[3] ^ IDBuf[4] ^ IDBuf[5]) & 0x01);
+
+	ID = ID | (P0 << 6) | (P1 << 7);
+	return ID;
+}
+
+uint8_t Checksum_Calc(uint8_t PID, uint8_t *data, uint8_t size)
+{
+	uint8_t buffer[size + 2];
+	uint16_t sum = 0;
+	buffer[0] = PID;
+	for (int i = 0; i < size; i++)
+	{
+		buffer[i + 1] = data[i];
+	}
+
+	for (int i = 0; i < size + 1; i++)
+	{
+		sum = sum + buffer[i];
+		if (sum > 0xff)
+			sum = sum - 0xff;
+	}
+
+	sum = 0xff - sum;
+	return sum;
+}
+
+static void showPartDatasheet(void)
+{
+	//const char *text = "Enjoy your life. There is no second take on it.";
+	const char *text = "https://allegro.pl/listing?string=5G1955119A";
+	enum qrcodegen_Ecc errCorLvl = qrcodegen_Ecc_LOW;  // Error correction level
+
+	// Make and print the QR Code symbol
+	uint8_t qrcode[qrcodegen_BUFFER_LEN_MAX];
+	uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
+	bool ok = qrcodegen_encodeText(text, tempBuffer, qrcode, errCorLvl,
+	qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
+	if (ok)
+		printQr(qrcode);
+	displayQr(qrcode);
+
+}
+
+static void printQr(const uint8_t qrcode[])
+{
+	int size = qrcodegen_getSize(qrcode);
+	int border = 4;
+	for (int y = -border; y < size + border; y++)
+	{
+		for (int x = -border; x < size + border; x++)
+		{
+			printf("%s", qrcodegen_getModule(qrcode, x, y) ? "##" : "  ");
+		}
+		printf("\r\n");
+	}
+	printf("\r\n");
+}
+
+static void displayQr(const uint8_t qrcode[])
+{
+	int size = qrcodegen_getSize(qrcode);
+	int border = 4;
+	ssd1306_Fill(Black);
+	for (int y = -border; y < size + border; y++)
+	{
+		for (int x = -border; x < size + border; x++)
+		{
+			if (qrcodegen_getModule(qrcode, x, y) == 1)
+			{
+				ssd1306_DrawPixel(2 * x + 40, 2 * y + 2, White);
+				ssd1306_DrawPixel(2 * x + 1 + 40, 2 * y + 1 + 2, White);
+				ssd1306_DrawPixel(2 * x + 40, 2 * y + 1 + 2, White);
+				ssd1306_DrawPixel(2 * x + 1 + 40, 2 * y + 2, White);
+				//ssd1306_FillRectangle(x, y, x+1, y+1,White);
+			}
+			//	printf("%s",qrcodegen_getModule(qrcode, x, y) ? "##" : "  ");
+		}
+		//	printf("\r\n");
+	}
+//	printf("\r\n");
+	ssd1306_UpdateScreen();
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ README.md
@@ -0,0 +1,54 @@
+# LIN bus demo using VW Golf VII / Audi A3 8V wiper actuator (STM32G474RE)
+My strong inclination towards electric drives made me knock together a sequel to the [Toyota RAV4 climate control panel](https://github.com/ufnalski/lin_bus_rav4_climate_control_g474re). We are going to communicate over the LIN bus with a wiper actuator. There is almost endless selection of such actuators on the market. However, my goal here is not to spend time on hacking one. The exercise is supposed to be a datasheet-based one. Searching for _wiper actuator/motor protocol_ gave me Wiper Direct Actuator (WDA) from Bosch Motorsport. A quite pricey item. Let's experiment with something cheaper - a similarly looking actuator being used in VW Golf VII, Audi A3 8V, and probably many other popular models. The used part can be easily bought under $10.
+
+![LIN bus wiper actuator in action](/Assets/Images/golf7_wda_in_action.jpg)
+
+![WDA purchasing options](/Assets/Images/golf7_wda_qr_code.JPG)
+
+# Missing files?
+Don't worry :slightly_smiling_face: Just hit Alt-K to generate /Drivers/CMCIS/ and /Drivers/STM32G4xx_HAL_Driver/ based on the .ioc file. After a couple of seconds your project will be ready for building.
+
+# Libraries
+* [stm32-ssd1306](https://github.com/afiskon/stm32-ssd1306) (MIT license)
+* [QR Code[^1] generator library](https://www.nayuki.io/page/qr-code-generator-library) (MIT license)
+
+[^1]: The QR code will take you to the wiper direct actuator (WDA) purchasing options.
+
+# Exemplary LIN transceivers
+* [Dual LIN click](https://www.mikroe.com/dual-lin-click) (MIKROE)
+* [LIN Click](https://www.mikroe.com/lin-click) (MIKROE)
+* [MCP2003B Click](https://www.mikroe.com/mcp2003b-click) (MIKROE)
+
+# Readings
+* [Local Interconnect Network](https://en.wikipedia.org/wiki/Local_Interconnect_Network) (Wikipedia)
+* [LIN Bus Explained - A Simple Intro [2023]](https://www.csselectronics.com/pages/lin-bus-protocol-intro-basics) (CSS Electronics)
+* [Introduction to the LIN bus](https://kvaser.com/about-can/can-standards/linbus/) (KVASER)
+* [STM32 as the Lin Master || Configure & Send Data](https://controllerstech.com/stm32-uart-8-lin-protocol-part-1/) (ControllersTech)
+* [Using Lin Transceivers to communicate between master and slave](https://controllerstech.com/stm32-uart-9-lin-protocol-part-2/) (ControllersTech)
+* [Master - slave communication using LinBus](https://controllerstech.com/stm32-uart-10-lin-protocol-part-3/) (ControllersTech)
+* [Wiper Direct Actuator WDA (datasheet with protocol description)](https://www.bosch-motorsport.com/content/downloads/Raceparts/Resources/pdf/Data%20Sheet_68715659_Wiper_Direct_Actuator_WDA.pdf) (Bosch Motorsport)
+* [LIN Specification Package Revision 2.2A](https://www.cs-group.de/wp-content/uploads/2016/11/LIN_Specification_Package_2.2A.pdf)
+* [VW Golf 7 sterowanie silnikiem wycieraczek przod siec LIN. BUS LIN.](https://www.youtube.com/watch?v=aTWoo6geNao) (eCarEdu.pl)
+
+> [!IMPORTANT]
+> The WDA for motorsport has visibly different casing but it uses an almost identical communication protocol. "Almost" makes a difference between being able to communicate and not being able to communicate at all. It took me a lot of guessing and checking to figure out that the "civilian" version has a longer data field. They've probably shortened the frame for motorsport to make it faster :wink: Follow the datasheet and then just add two zero bytes to the payload.
+
+# Exemplary sniffers/decoders
+* [DSLogic Plus](https://www.dreamsourcelab.com/product/dslogic-series/) (DreamSourceLab)
+* [PLIN-USB: LIN Interface for USB](https://www.peak-system.com/PLIN-USB.485.0.html?&L=1) (PEAK-System)
+
+![LIN bus decoder](/Assets/Images/dsview_linbus_golf7_wda.JPG)
+![LIN bus interface/sniffer](/Assets/Images/plin_golf7_wda.JPG)
+
+# For ESP32 (Arduino IDE) afficionados
+* [ESP32-SoftwareLIN](https://github.com/CW-B-W/ESP32-SoftwareLIN) (a nice read about the break field)
+
+# Call for action
+Create your own [home laboratory/workshop/garage](http://ufnalski.edu.pl/control_engineering_for_hobbyists/2024_dzien_popularyzacji_matematyki/Dzien_Popularyzacji_Matematyki_2024.pdf)! Get inspired by [ControllersTech](https://www.youtube.com/@ControllersTech), [DroneBot Workshop](https://www.youtube.com/@Dronebotworkshop), [Andreas Spiess](https://www.youtube.com/@AndreasSpiess), [GreatScott!](https://www.youtube.com/@greatscottlab), [ElectroBOOM](https://www.youtube.com/@ElectroBOOM), [Phil's Lab](https://www.youtube.com/@PhilsLab), [atomic14](https://www.youtube.com/@atomic14), [That Project](https://www.youtube.com/@ThatProject), [Paul McWhorter](https://www.youtube.com/@paulmcwhorter), [Max Imagination](https://www.youtube.com/@MaxImagination), [Nikodem Bartnik](https://www.youtube.com/@nikodembartnik), and many other professional hobbyists sharing their awesome projects and tutorials! Shout-out/kudos to all of them!
+
+> [!WARNING]
+> Control engineering - do try this at home :sunglasses:
+
+190+ challenges to start from: [Control Engineering for Hobbyists at the Warsaw University of Technology](http://ufnalski.edu.pl/control_engineering_for_hobbyists/Control_Engineering_for_Hobbyists_list_of_challenges.pdf).
+
+Stay tuned!
